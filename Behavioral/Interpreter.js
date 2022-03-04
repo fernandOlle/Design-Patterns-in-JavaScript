10ce8f5d2e182ba1c7f936ba7e5088d59dc7bb97
fernandOlle <frolle@inf.ufpel.edu.br>
2022-03-03 23:46:14 -0300

    Add Interpreter

---
+++ Behavioral/Interpreter.js
@@ -0,0 +1,53 @@
+class ExpressionProcessor {
+  constructor() {
+    this.variables = {};
+    this.nextOp = Object.freeze({
+      nothing: 0,
+      plus: 1,
+      minus: 2,
+    });
+  }
+
+  calculate(expression) {
+    let current = 0;
+    let nextOp = this.nextOp.nothing;
+
+    let parts = expression.split(/(?<=[+-])/);
+
+    for (let part of parts) {
+      let noop = part.split('+-');
+      let first = noop[0];
+      let value = 0,
+        z = 0;
+
+      z = parseInt(first);
+      if (!isNaN(z)) value = z;
+      else if (first.length === 1 && this.variables[first[0]] !== undefined) {
+        value = this.variables[first[0]];
+      } else return 0;
+
+      switch (nextOp) {
+        case this.nextOp.nothing:
+          current = value;
+          break;
+        case this.nextOp.plus:
+          current += value;
+          break;
+        case this.nextOp.minus:
+          current -= value;
+          break;
+      }
+
+      if (part.endsWith('+')) nextOp = this.nextOp.plus;
+      else if (part.endsWith('-')) nextOp = this.nextOp.minus;
+    }
+    return current;
+  }
+}
+
+let ep = new ExpressionProcessor();
+ep.variables['x'] = 3;
+
+console.log(ep.calculate('1+2+3'));
+console.log(ep.calculate('1+2+xy'));
+console.log(ep.calculate('10-2-x'));
